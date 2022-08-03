70482f3dd9ee95f01c18dc3dcc3043f6b388a2bc
Arsinnn <90065236+Arsinnn@users.noreply.github.com>
2022-08-03 02:33:37 -0400

    Joe

---
+++ FlyKeybind/Main.cs
@@ -0,0 +1,19 @@
+﻿using ABI_RC.Systems.MovementSystem;
+using MelonLoader;
+using UnityEngine;
+
+namespace FlyKeybind
+{
+    public class Main : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.F))
+            {
+                MovementSystem.Instance.canFly = true;
+                MovementSystem.Instance.ToggleFlight();
+            }
+
+        }
+    }
+}
---
+++ FlyKeybind/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyProduct("JoeMod")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("JoeMod")]
+[assembly: AssemblyFileVersion("4.9.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: MelonGame(null, null)]
+[assembly: AssemblyTitle("JoeMod")]
+[assembly: AssemblyDescription("")]
+[assembly: MelonInfo(typeof(FlyKeybind.Main), "FlyKeyBind", "1.0.0", "arsin#9999", null)]
+[assembly: Guid("52d278b5-c4e4-45c3-b827-f3d741399698")]
